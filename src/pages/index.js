bc25f76b1a59e703428718ba3d25303f2bb4bbaf
RyzorBent <zestadaryzor@gmail.com>
2022-02-21 08:24:15 +0200

    web under construction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+return <div><h1>The Village Farm<h1><hr/><h2>Website Under Construction</h2></div>
 }
